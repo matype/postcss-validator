e44bd5117fab1e8c080f0f02cc69c4de9ec74073
morishitter <morishitter@gmail.com>
2017-03-14 23:05:35 +0900

    add test code

---
+++ test/index.js
@@ -0,0 +1,37 @@
+var fs = require('fs')
+var test = require('tape')
+var postcss = require('postcss')
+var validator = require('..')
+
+function fixture (name) {
+  return fs.readFileSync('test/fixtures/' + name + '.css', 'utf-8').trim()
+}
+
+function run (name, options) {
+  options = options || {}
+  return postcss().use(validator()).process(fixture(name)).css.trim()
+}
+
+test('valid', function (t) {
+  t.doesNotThrow(function () {
+    run('valid')
+  })
+  t.end()
+})
+
+test('nested selector', function (t) {
+  t.throws(function () {
+    run('nested-selector')
+  },
+  'ested rules [.nested]')
+  t.end()
+})
+
+test('undefined property', function (t) {
+  t.throws(function () {
+    run('undefined-property')
+  },
+  'Unknown property [hoge] is used')
+  t.end()
+})
+
