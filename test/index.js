d5882ee22584dcb21e19845fbc8306d3d4f82198
morishitter <morishitter@gmail.com>
2017-03-15 15:44:10 +0900

    fix test code

--- test/index.js
+++ test/index.js
@@ -9,7 +9,7 @@ function fixture (name) {
 
 function run (name, options) {
   options = options || {}
-  return postcss().use(validator({unknownProperty: false})).process(fixture(name)).css
+  return postcss().use(validator()).process(fixture(name)).css
 }
 
 test('valid', function (t) {
